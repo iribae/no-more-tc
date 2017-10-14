f9b24e3869e622105e3ebc36d9b80325eb9d1371
iribae <xiphion.iris@gmail.com>
2017-10-14 21:29:53 +0200

    being all fancy like

--- no-more-tc.js
+++ no-more-tc.js
@@ -1,5 +1,10 @@
+const Command = require('command')
 module.exports = function nomoretc(dispatch) {
-	dispatch.hook('S_ABNORMALITY_BEGIN', 2, event => {
+	const command = Command(dispatch)
+	let enabled = true
+	command.add('tcblock',() => { enabled = !enabled})
+	dispatch.hook('S_ABNORMALITY_BEGIN', 2, (event) => {
+		if(!enabled){return true}
 		if(id=101300) {return false}
 	})
 }
