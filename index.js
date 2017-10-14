a3a1d42c62a15b515b1ccb695eff6d25cf1dd063
iribae <xiphion.iris@gmail.com>
2017-10-14 21:57:18 +0200

    even added a message for the command!

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const Command = require('command')
 module.exports = function nomoretc(dispatch) {
 	const command = Command(dispatch)
 	let enabled = true
-	command.add('tcblock', () => { enabled = !enabled})
+	command.add('tcblock', () => {
+		enabled = !enabled
+		command.message('best tank buff spam blocker '+(enabled?'enabled':'disabled')+'.')
+	})
 	dispatch.hook('S_ABNORMALITY_BEGIN', 2, (event) => {
 		if(!enabled){return true}
 		if(id=101300) {return false}
